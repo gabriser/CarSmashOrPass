a8d55743be94e1771d5d86cae83416c734d27e87
gabriser <gabrielserrat2015@gmail.com>
2024-05-27 16:23:38 +0200

    Changes: - changed github link from gabriser profile to CarSmashOrPass repository. - added src/db/cars.json 5 cars info demo. - added public/cars_img the images from the cars info demo. - updated Credits for the new assets 'cars info demo' from 'Forza Wiki'. - updated PlayGame with the initial design of the card and the buttons Smash or Pass. Static info.

--- carsmashorpass/src/components/PlayGame/index.jsx
+++ carsmashorpass/src/components/PlayGame/index.jsx
@@ -5,9 +5,45 @@ function PlayGame() {
         
         <main id={styles.playgame}>
 
+            <div id={styles.card}>
+                <h1>Titulo aaa</h1>
+                <img src='cars_img/1.webp' alt='' title='' />
+                <div id={styles.details}>
+                    <div className={styles.detcol}>
+                        <h2>Brand</h2>
+                        <h3>aaa</h3>
+                    </div>
+                    <div className={styles.detcol}>
+                        <h2>Model</h2>
+                        <h3>aaa</h3>
+                    </div>
+                    <div className={styles.detcol}>
+                        <h2>Year</h2>
+                        <h3>aaa</h3>
+                    </div>
+                    <div className={styles.detcol}>
+                        <h2>Country</h2>
+                        <h3>aaa</h3>
+                    </div>
+                </div>
+            </div>
+
+            <section id={styles.contbtn}>
+                <button id={styles.smashbtn} onClick={(e)=>SmashCar()} >Smash</button>
+                <button id={styles.passbtn} onClick={(e)=>PassCar()}>Pass</button>
+            </section>
+
         </main>
         
     )
+}
+
+function SmashCar() {
+    alert("smash");
+}
+
+function PassCar() {
+    alert("pass");
 }
 
 export default PlayGame
