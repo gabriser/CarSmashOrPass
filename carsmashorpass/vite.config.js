c0b18827e8d9f8edf4c3f76cac46c900988dd104
gabriser <gabrielserrat2015@gmail.com>
2024-06-11 14:55:02 +0200

    New Mazda cars (MX5, RX7-8): - '1994 Mazda MX-5 Miata', '2005 Mazda Mazdaspeed MX-5', '2013 Mazda MX-5', '2016 Mazda MX-5' - '1990 Mazda Savanna RX-7', '1997 Mazda RX-7', '2002 Mazda RX-7 Spirit R Type-A', '2011 Mazda RX-8 R3'

--- carsmashorpass/vite.config.js
+++ carsmashorpass/vite.config.js
@@ -5,3 +5,15 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
 })
+
+// exponer hacia fuera de local (solo router)
+/*import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+
+export default defineConfig({
+    plugins: [react()],
+    server: {
+        host: true,
+        port: 5173, // Puedes cambiar el puerto si lo deseas
+    },
+});*/
