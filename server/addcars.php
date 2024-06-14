fc4a26433ecd9d079476cea89a7cc2207df646e8
gabriser <gabrielserrat2015@gmail.com>
2024-06-14 17:19:48 +0200

    Updated addcars.php script and 6 new Nissan cars: - addcars.php script updated with the new columns in the cars.db sqlite. - 6 new Nissan (Silvia, Sentra) cars: - - '1992 Nissan Silvia CLUB K's', '1994 Nissan Silvia K's', '1998 Nissan Silvia K's AERO', '2000 Nissan Silvia Spec-R'. - - '2010 Nissan Tsuru (Sentra B13)', '2018 Nissan Sentra Nismo'.

--- server/addcars.php
+++ server/addcars.php
@@ -27,12 +27,16 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $model = $_POST['model'];
     $year = $_POST['year'];
     $country = $_POST['country'];
+    $engine = $_POST['engine'];
     $power = $_POST['power'];
+    $layout = $_POST['layout'];
+    $transmission = $_POST['transmission'];
+    $weight = $_POST['weight'];
     $moreInfo = $_POST['moreInfo'];
 
     // Insertamos los datos en la base de datos
-    $stmt = $db->prepare('INSERT INTO cars (brand, model, year, country, power, moreinfo, smash, pass) VALUES (?, ?, ?, ?, ?, ?, 0, 0)');
-    $stmt->execute([$brand, $model, $year, $country, $power, $moreInfo]);
+    $stmt = $db->prepare('INSERT INTO cars (brand, model, year, country, engine, power, layout, transmission, weight, moreinfo, smash, pass) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, 0, 0)');
+    $stmt->execute([$brand, $model, $year, $country, $engine, $power, $layout, $transmission, $weight, $moreInfo]);
 
     echo "<pre>";
     var_dump($_POST);
@@ -47,6 +51,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Add Car</title>
+    <style>
+        input {
+            width: 200px;
+        }
+    </style>
 </head>
 <body>
 
@@ -58,22 +67,35 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <form method="post">
     <label for="brand">Brand:</label>
-    <input type="text" id="brand" name="brand" required><br><br>
+    <input type="text" id="brand" name="brand" placeholder="ex: 'Honda'" required><br><br>
 
     <label for="model">Model:</label>
-    <input type="text" id="model" name="model" required><br><br>
+    <input type="text" id="model" name="model" placeholder="ex: 'Civic Type R'" required><br><br>
 
     <label for="year">Year:</label>
-    <input type="number" id="year" name="year" required><br><br>
+    <input type="number" id="year" name="year" placeholder="ex: '1997'" required><br><br>
 
     <label for="country">Country:</label>
-    <input type="text" id="country" name="country" required><br><br>
+    <input type="text" id="country" name="country" placeholder="ex: 'Japan'" required><br><br>
+
+    <label for="engine">Engine:</label>
+    <input type="text" id="engine" name="engine" placeholder="ex: '1.6L Naturally-Aspirated I4'" required><br><br>
 
     <label for="power">Power:</label>
-    <input type="number" id="power" name="power" required><br><br>
+    <input type="number" id="power" name="power" placeholder="ex: '182' bhp" required><br><br>
+
+    <label for="layout">Layout:</label>
+    <input type="text" id="layout" name="layout" placeholder="ex: 'FE FWD'" required><br><br>
+
+    <label for="transmission">Transmission:</label>
+    <input type="number" id="transmission" name="transmission" placeholder="ex: '5' speed" required><br><br>
+
+    <label for="weight">Weight:</label>
+    <input type="number" id="weight" name="weight" placeholder="ex: '1090' kg" required><br><br>
 
     <label for="moreInfo">More Info (Forza Wiki car link):</label><br>
-    <textarea id="moreInfo" name="moreInfo" rows="4" cols="50"></textarea><br><br>
+    <textarea id="moreInfo" name="moreInfo" rows="4" cols="50" 
+    placeholder="ex: 'https://forza.fandom.com/wiki/Honda_Civic_Type_R_(1997)'" required></textarea><br><br>
 
     <input type="submit" value="Submit">
 </form>
