83fc174814cdc838aa19e23eab33a3955332958e
gabriser <gabrielserrat2015@gmail.com>
2024-06-15 16:30:28 +0200

    New feature (Card Card on Scoreboard) and small changes: - Scoreboard: Now the link for each car name does not open the forza wiki page, but instead shows the complete car card. - Credits: Small change on technologies display flex and jusfify-content space-evenly. - PlayGame: Small change on 'Go back to game' btn. Text changed to 'Back to Game'. - PlayGame: Small change on 'Show history' btn. Text changed to 'See History'.

--- carsmashorpass/src/components/PlayGame/index.jsx
+++ carsmashorpass/src/components/PlayGame/index.jsx
@@ -120,7 +120,7 @@ function PlayGame() {
                     <section id={styles.contbackgamebtn}>
                         <button id={styles.backgamebtn} onClick={() => setShowHistory(false)}>
                             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M73 39c-14.8-9.1-33.4-9.4-48.5-.9S0 62.6 0 80V432c0 17.4 9.4 33.4 24.5 41.9s33.7 8.1 48.5-.9L361 297c14.3-8.7 23-24.2 23-41s-8.7-32.2-23-41L73 39z"/></svg>
-                            Go back to game
+                            Back to Game
                         </button>
                     </section>
                 </section>
@@ -182,7 +182,7 @@ function PlayGame() {
             <section id={styles.conthistorybtn}>
                 <button id={styles.historybtn} onClick={() => setShowHistory(true)}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M75 75L41 41C25.9 25.9 0 36.6 0 57.9V168c0 13.3 10.7 24 24 24H134.1c21.4 0 32.1-25.9 17-41l-30.8-30.8C155 85.5 203 64 256 64c106 0 192 86 192 192s-86 192-192 192c-40.8 0-78.6-12.7-109.7-34.4c-14.5-10.1-34.4-6.6-44.6 7.9s-6.6 34.4 7.9 44.6C151.2 495 201.7 512 256 512c141.4 0 256-114.6 256-256S397.4 0 256 0C185.3 0 121.3 28.7 75 75zm181 53c-13.3 0-24 10.7-24 24V256c0 6.4 2.5 12.5 7 17l72 72c9.4 9.4 24.6 9.4 33.9 0s9.4-24.6 0-33.9l-65-65V152c0-13.3-10.7-24-24-24z"/></svg>
-                    See history
+                    See History
                     </button>
             </section>
         </main>
--- carsmashorpass/src/components/Scoreboard/index.jsx
+++ carsmashorpass/src/components/Scoreboard/index.jsx
@@ -7,6 +7,7 @@ function Scoreboard() {
     const [smashedCars, setSmashedCars] = useState([]);
     const [passedCars, setPassedCars] = useState([]);
     const [allCars, setAllCars] = useState([]);
+    const [selectedCar, setSelectedCar] = useState(null); // Estado para el coche seleccionado
     let i = 1;
 
     const fetchSmashedCars = async () => {
@@ -42,94 +43,155 @@ function Scoreboard() {
         );
     }
 
+    const renderCarCard = (car) => (
+        <div className={`${styles.card}`}>
+            <h1><span>{car.brand}</span> {car.model}</h1>
+            <img src={`cars_img/${car.id}.webp`} alt={`${car.brand} ${car.model}`} />
+            <div id={styles.detflex}>
+                <div className={styles.details}>
+                    <div className={styles.detcol}>
+                        <h2>Year</h2>
+                        <h3>{car.year}</h3>
+                    </div>
+                    <div className={styles.detcol}>
+                        <h2>Country</h2>
+                        <h3>{car.country}</h3>
+                    </div>
+                    <div className={styles.detcol}>
+                        <h2>Engine</h2>
+                        <h3>{car.engine}</h3>
+                    </div>
+                    <div className={styles.detcol}>
+                        <h2>Power</h2>
+                        <h3>{car.power} bhp</h3>
+                    </div>
+                </div>
+                <div className={styles.details}>
+                    <div className={styles.detcol}>
+                        <h2>Layout</h2>
+                        <h3>{car.layout}</h3>
+                    </div>
+                    <div className={styles.detcol}>
+                        <h2>Transmission</h2>
+                        <h3>{car.transmission} speed</h3>
+                    </div>
+                    <div className={styles.detcol}>
+                        <h2>Weight</h2>
+                        <h3>{car.weight} kg</h3>
+                    </div>
+                    <div className={styles.detcol}>
+                        <Link to={car.moreinfo} target="_blank" className={styles.moreinfo}>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z"/></svg>
+                            Open Forza Wiki
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+
     return (
         <main id={styles.scoreboard}>
-            <h1>Scoreboard</h1>
 
-            <section id={styles.contbtn}>
-                <button id={styles.smashbtn} onClick={() => setView('smashed')}>Most Smashed</button>
-                <button id={styles.allbtn} onClick={() => setView('all')}>All</button>
-                <button id={styles.passbtn} onClick={() => setView('passed')}>Most Passed</button>
-            </section>
-
-            {view === 'smashed' && (
-                <section id={styles.smashtable}>
-                    <h2>Most Smashed</h2>
-                    <div className={styles.tablescroll}>
-                        <table>
-                            <tbody>
-                                {smashedCars.map(car => (
-                                    <tr key={car.id}>
-                                        <td>{i++}</td>
-                                        <td><img src={`cars_img/${car.id}.webp`} alt={`${car.year} ${car.brand} ${car.model}`} /></td>
-                                        <td>
-                                            <Link to={car.moreinfo} target="_blank" className={styles.moreinfo}>
-                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z"/></svg>
-                                                {car.year} {car.brand} {car.model}
-                                            </Link>
-                                        </td>
-                                        <td>{car.smash}</td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    </div>
+            {selectedCar ? (
+                <section id={styles.cardetail}>
+                    <h1>Car Card</h1>
+                    {renderCarCard(selectedCar)}
+                    <section id={styles.contbackscoreboardbtn}>
+                        <button id={styles.backscoreboardbtn} onClick={() => setSelectedCar(null)}>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path d="M0 96C0 60.7 28.7 32 64 32H512c35.3 0 64 28.7 64 64V416c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V96zM128 288a32 32 0 1 0 0-64 32 32 0 1 0 0 64zm32-128a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zM128 384a32 32 0 1 0 0-64 32 32 0 1 0 0 64zm96-248c-13.3 0-24 10.7-24 24s10.7 24 24 24H448c13.3 0 24-10.7 24-24s-10.7-24-24-24H224zm0 96c-13.3 0-24 10.7-24 24s10.7 24 24 24H448c13.3 0 24-10.7 24-24s-10.7-24-24-24H224zm0 96c-13.3 0-24 10.7-24 24s10.7 24 24 24H448c13.3 0 24-10.7 24-24s-10.7-24-24-24H224z" /></svg>
+                            Back to Scoreboard
+                        </button>
+                    </section>
                 </section>
-            )}
+            ) : (
+                <>
+                    <h1>Scoreboard</h1>
 
-            {view === 'passed' && (
-                <section id={styles.passtable}>
-                    <h2>Most Passed</h2>
-                    <div className={styles.tablescroll}>
-                        <table>
-                            <tbody>
-                                {passedCars.map(car => (
-                                    <tr key={car.id}>
-                                        <td>{i++}</td>
-                                        <td><img src={`cars_img/${car.id}.webp`} alt={`${car.year} ${car.brand} ${car.model}`} /></td>
-                                        <td>
-                                            <Link to={car.moreinfo} target="_blank" className={styles.moreinfo}>
-                                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z"/></svg>
-                                                {car.year} {car.brand} {car.model}
-                                            </Link>
-                                        </td>
-                                        <td>{car.pass}</td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    </div>
-                </section>
-            )}
+                    <section id={styles.contbtn}>
+                        <button id={styles.smashbtn} onClick={() => setView('smashed')}>Most Smashed</button>
+                        <button id={styles.allbtn} onClick={() => setView('all')}>All</button>
+                        <button id={styles.passbtn} onClick={() => setView('passed')}>Most Passed</button>
+                    </section>
 
-            {view === 'all' && (
-                <section id={styles.alltable}>
-                    <h2>All Cars</h2>
-                    <div className={styles.tablescroll}>
-                        <table>
-                            <tbody>
-                                {allCars.map(car => {
-                                    return (
-                                        <tr key={car.id}>
-                                            <td>{i++}</td>
-                                            <td><img src={`cars_img/${car.id}.webp`} alt={`${car.year} ${car.brand} ${car.model}`} /></td>
-                                            <td>
-                                                <Link to={car.moreinfo} target="_blank" className={styles.moreinfo}>
-                                                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z"/></svg>
-                                                    {car.year} {car.brand} {car.model}
-                                                </Link>
-                                            </td>
-                                            <td>
-                                                <span id={styles.allsmash}>{car.smash}</span><br/>
-                                                <span id={styles.allpass}>{car.pass}</span>
-                                            </td>
-                                        </tr>
-                                    );
-                                })}
-                            </tbody>
-                        </table>
-                    </div>
-                </section>
+                    {view === 'smashed' && (
+                        <section id={styles.smashtable}>
+                            <h2>Most Smashed</h2>
+                            <div className={styles.tablescroll}>
+                                <table>
+                                    <tbody>
+                                        {smashedCars.map(car => (
+                                            <tr key={car.id}>
+                                                <td>{i++}</td>
+                                                <td><img src={`cars_img/${car.id}.webp`} alt={`${car.year} ${car.brand} ${car.model}`} /></td>
+                                                <td>
+                                                    <span onClick={() => setSelectedCar(car)} className={styles.moreinfo}>
+                                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M135.2 117.4L109.1 192H402.9l-26.1-74.6C372.3 104.6 360.2 96 346.6 96H165.4c-13.6 0-25.7 8.6-30.2 21.4zM39.6 196.8L74.8 96.3C88.3 57.8 124.6 32 165.4 32H346.6c40.8 0 77.1 25.8 90.6 64.3l35.2 100.5c23.2 9.6 39.6 32.5 39.6 59.2V400v48c0 17.7-14.3 32-32 32H448c-17.7 0-32-14.3-32-32V400H96v48c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32V400 256c0-26.7 16.4-49.6 39.6-59.2zM128 288a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zm288 32a32 32 0 1 0 0-64 32 32 0 1 0 0 64z"/></svg>
+                                                        {car.year} {car.brand} {car.model}
+                                                    </span>
+                                                </td>
+                                                <td>{car.smash}</td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            </div>
+                        </section>
+                    )}
+
+                    {view === 'passed' && (
+                        <section id={styles.passtable}>
+                            <h2>Most Passed</h2>
+                            <div className={styles.tablescroll}>
+                                <table>
+                                    <tbody>
+                                        {passedCars.map(car => (
+                                            <tr key={car.id}>
+                                                <td>{i++}</td>
+                                                <td><img src={`cars_img/${car.id}.webp`} alt={`${car.year} ${car.brand} ${car.model}`} /></td>
+                                                <td>
+                                                    <span onClick={() => setSelectedCar(car)} className={styles.moreinfo}>
+                                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M135.2 117.4L109.1 192H402.9l-26.1-74.6C372.3 104.6 360.2 96 346.6 96H165.4c-13.6 0-25.7 8.6-30.2 21.4zM39.6 196.8L74.8 96.3C88.3 57.8 124.6 32 165.4 32H346.6c40.8 0 77.1 25.8 90.6 64.3l35.2 100.5c23.2 9.6 39.6 32.5 39.6 59.2V400v48c0 17.7-14.3 32-32 32H448c-17.7 0-32-14.3-32-32V400H96v48c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32V400 256c0-26.7 16.4-49.6 39.6-59.2zM128 288a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zm288 32a32 32 0 1 0 0-64 32 32 0 1 0 0 64z"/></svg>
+                                                        {car.year} {car.brand} {car.model}
+                                                    </span>
+                                                </td>
+                                                <td>{car.pass}</td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            </div>
+                        </section>
+                    )}
+
+                    {view === 'all' && (
+                        <section id={styles.alltable}>
+                            <h2>All Cars</h2>
+                            <div className={styles.tablescroll}>
+                                <table>
+                                    <tbody>
+                                        {allCars.map(car => (
+                                            <tr key={car.id}>
+                                                <td>{i++}</td>
+                                                <td><img src={`cars_img/${car.id}.webp`} alt={`${car.year} ${car.brand} ${car.model}`} /></td>
+                                                <td>
+                                                    <span onClick={() => setSelectedCar(car)} className={styles.moreinfo}>
+                                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M135.2 117.4L109.1 192H402.9l-26.1-74.6C372.3 104.6 360.2 96 346.6 96H165.4c-13.6 0-25.7 8.6-30.2 21.4zM39.6 196.8L74.8 96.3C88.3 57.8 124.6 32 165.4 32H346.6c40.8 0 77.1 25.8 90.6 64.3l35.2 100.5c23.2 9.6 39.6 32.5 39.6 59.2V400v48c0 17.7-14.3 32-32 32H448c-17.7 0-32-14.3-32-32V400H96v48c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32V400 256c0-26.7 16.4-49.6 39.6-59.2zM128 288a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zm288 32a32 32 0 1 0 0-64 32 32 0 1 0 0 64z"/></svg>
+                                                        {car.year} {car.brand} {car.model}
+                                                    </span>
+                                                </td>
+                                                <td>
+                                                    <span id={styles.allsmash}>{car.smash}</span><br/>
+                                                    <span id={styles.allpass}>{car.pass}</span>
+                                                </td>
+                                            </tr>
+                                        ))}
+                                    </tbody>
+                                </table>
+                            </div>
+                        </section>
+                    )}
+                </>
             )}
         </main>
     );
